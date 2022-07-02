b77474a4fa6b09c57b77d4b005eeeace48e4b151
Oumayma TABBAZA <oumaymatb@gmail.com>
2022-07-02 02:34:14 +0200

    change

--- RootSearch_unedited.cpp
+++ RootSearch_unedited.cpp
@@ -85,8 +85,8 @@ int main()
 	double vol_level = 0.0855;
 	double correl = -0.315;
     double maturity = 2.;
-    double strike = 120.;
-    double implied_vol = 0.28;
+    double strike = 70.;
+    double implied_vol = 0.52;
 	cout << "BS call for strike " << strike << " is " << BS_call(init_spot, strike, risk_free_rate, implied_vol,maturity);
 
 }
